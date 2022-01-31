cb4c94affce0a182ab7d39d18d34ed56b731716d
Youssef-Roshdy <youssef.roshdy@vcloud.com>
2022-01-31 22:59:54 +0200

    correct twilio link

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 // PRODUCTION
-// export const SERVER_BASE = 'https://vc-server-convetec-1881-dev.twil.iog';
+// export const SERVER_BASE = 'https://vc-server-convetec-1881-dev.twil.io';
 // export const LARAVEL_CRM_URL = 'https://convatec-outbound-crm-dot-vci-live.uc.r.appspot.com/';
 // export const CORE_CRM_URL = 'https://convatec-outbound-crm-dot-vci-live.uc.r.appspot.com/';
 
 // TESTING
-export const SERVER_BASE = 'https://vc-server-convetec-1881-dev.twil.iog';
+export const SERVER_BASE = 'https://vc-server-convetec-1881-dev.twil.io';
 export const LARAVEL_CRM_URL = 'https://convatec-outbound-crm-test-dot-vci-live.uc.r.appspot.com/';
 export const CORE_CRM_URL = 'https://convatec-outbound-crm-test-dot-vci-live.uc.r.appspot.com/';
